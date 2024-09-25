37d5c7417d26381bcc0c143731e82f452a7afdb2
Ersei Saggi <vcs@ersei.net>
2024-09-25 18:22:45 -0400

    Have authentication middleware return JSON errors

--- backend/app/middleware/token_auth_middleware.py
+++ backend/app/middleware/token_auth_middleware.py
@@ -20,18 +20,18 @@ def require_logged_in_user(next_fn: AuthFunction) -> Callable[..., Tuple[Any, in
         # Extract the bearer auth
         token_to_check = request.headers.get("Authorization")
         if token_to_check is None:
-            return "No authentication provided", 403
+            return {"err": "No authentication provided"}, 403
         token_to_check = token_to_check.strip()
         # This token is prefixed with "Bearer", so we have to trim that
         split_token = token_to_check.split(" ")
         if len(split_token) != 2:
-            return "Bad authentication", 400
+            return {"err": "Bad authentication"}, 400
         match validate_token_and_refresh(split_token[1]):
             case Ok(user_id):
                 kwargs["user_id"] = user_id
                 kwargs["token"] = split_token[1]
                 return next_fn(*args, **kwargs)
             case Err(e):
-                return e, 401
+                return {"err": e}, 401
 
     return wrapper
