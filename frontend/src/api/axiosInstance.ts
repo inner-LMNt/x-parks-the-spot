9e4be2a8b68ba29b7300981fbb30d02832da4095
pkurto16 <ptkurto3@gmail.com>
2024-09-24 20:06:20 -0400

    Update live api endpoint

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/v1',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api/unstable',
 });
 
 export default axiosInstance;
