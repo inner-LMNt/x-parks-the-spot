33d8a711089a9638b75a14d9de875d1977545a2d
Ersei Saggi <vcs@ersei.net>
2024-10-06 21:06:10 -0400

    Deploy frontend as a container, I give up

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', reactStrictMode: false }
+const nextConfig = { output: 'standalone', reactStrictMode: false }
 
 export default nextConfig
