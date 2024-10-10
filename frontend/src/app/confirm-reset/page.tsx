d8e62437649162414bc83170e21f53338d224b04
pkurto16 <ptkurto3@gmail.com>
2024-10-09 20:21:37 -0400

    Quick redirect fix

--- frontend/src/app/confirm-reset/page.tsx
+++ frontend/src/app/confirm-reset/page.tsx
@@ -1,2 +1,17 @@
-import login from "../login/LoginPage"
-export default login
+'use client';
+
+import React, {useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+
+export default function RedirectPage() {
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/login');
+    }, []);
+
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4 overflow-hidden">
+        </div>
+    );
+}
