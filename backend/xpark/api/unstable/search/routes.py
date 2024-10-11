0109cae57c74df6264336dd2639f7e7cdae34fdc
pkurto16 <ptkurto3@gmail.com>
2024-10-10 20:30:36 -0400

    Starting search

---
+++ backend/xpark/api/unstable/search/routes.py
@@ -0,0 +1,23 @@
+from . import bp
+from xpark.logic.search import (
+    search_query,
+)
+from flask import request
+from typing import Tuple, Any
+
+@bp.post("")
+def search() -> Tuple[Any, int]:
+    search_query = {
+        "lat": float,
+        "long": float,
+        "radius": float,
+
+    }
+    return (
+        search_query(
+            lat=request.json["lat"],  # type: ignore
+            long=request.json["long"],  # type: ignore
+            radius_meters=request.json["radius"],  # type: ignore
+        ),
+        200,
+    )
