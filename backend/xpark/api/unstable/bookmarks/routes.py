e04aaf1928470e78a05da00f065214f19e013abc
Ersei Saggi <vcs@ersei.net>
2024-11-19 16:27:59 -0500

    Mypy

--- backend/xpark/api/unstable/bookmarks/routes.py
+++ backend/xpark/api/unstable/bookmarks/routes.py
@@ -4,9 +4,14 @@ from xpark.logic.parkingspace import get_bookmarked_spots
 from xpark.middleware.token_auth_middleware import require_logged_in_user
 from typing import Tuple, Any
 import uuid
+from result import Ok, Err
 
 
 @bp.get("")
 @require_logged_in_user
 def get_bookmarked_spots_route(token: str, user_id: uuid.UUID) -> Tuple[Any, int]:
-    return get_bookmarked_spots(user_id).ok_value, 200
+    match get_bookmarked_spots(user_id):
+        case Ok(spots):
+            return spots, 200
+        case Err(e):
+            return {"err": e}, 401
