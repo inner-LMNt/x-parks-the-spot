f497bb217d00ca75d08376b402a02230b2e01fb0
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2024-11-21 00:09:26 -0500

    tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,36 @@
 import type { Config } from 'tailwindcss';
 import animatePlugin from 'tailwindcss-animate';
 
-const config: Config = {
+const config: {
+	plugins: { handler: () => void }[];
+	theme: {
+		extend: {
+			keyframes: {
+				"accordion-up": { from: { height: string }; to: { height: string } };
+				"accordion-down": { from: { height: string }; to: { height: string } }
+			};
+			borderRadius: { md: string; sm: string; lg: string };
+			colors: {
+				border: string;
+				ring: string;
+				popover: { foreground: string; DEFAULT: string };
+				foreground: string;
+				accent: { foreground: string; DEFAULT: string };
+				destructive: { foreground: string; DEFAULT: string };
+				secondary: { foreground: string; DEFAULT: string };
+				input: string;
+				background: string;
+				muted: { foreground: string; DEFAULT: string };
+				chart: { "1": string; "2": string; "3": string; "4": string; "5": string };
+				card: { foreground: string; DEFAULT: string };
+				primary: { foreground: string; DEFAULT: string }
+			};
+			animation: { "accordion-up": string; "accordion-down": string }
+		}
+	};
+	darkMode: string[];
+	content: string[]
+} = {
 	darkMode: ['class'],
 	content: [
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
