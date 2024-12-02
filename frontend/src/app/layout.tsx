d6d68d6b6e24872c8e96f4c5a9433a621b048a5a
pkurto16 <ptkurto3@gmail.com>
2024-12-02 16:15:48 -0500

    Changes to try getting it working for ios correctly

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,10 @@
-// app/layout.tsx
-
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { Providers } from "./providers"
 import NavBarWrapper from "@/components/custom/nav-bar-wrapper"
 import { Toaster } from "@/components/ui/toaster"
 import NotificationBanner from "@/components/custom/NotificationBanner"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +17,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, user-scalable=no"
+        />
+        <meta name="theme-color" content="#ff9238" />
+        <meta name="description" content="Your solution to easy parking." />
+        <link rel="manifest" href="/manifest.ts" />
+        <link rel="icon" href="/icon512_rounded.png" />
+      </Head>
       <body className={inter.className}>
         <Providers>
           <NotificationBanner />
