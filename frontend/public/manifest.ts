23756bc8717c11ce979715808de5c99b14588cae
pkurto16 <ptkurto3@gmail.com>
2024-12-02 12:14:41 -0500

    Change to manifest

---
+++ frontend/public/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "X Parks the Spot",
+    short_name: "xpark",
+    description: "Parking anywhere. Rent and find paid or free spots.",
+    theme_color: "#ff9238",
+    background_color: "#ff80f4",
+    display: "fullscreen",
+    orientation: "portrait",
+    lang: "en-US",
+    dir: "auto",
+    icons: [
+      {
+        src: "icon512_maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "icon512_rounded.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+    start_url: "/",
+    scope: "/",
+  }
+}
