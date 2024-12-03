9b2bd7a3c7bc142c6f22be6a021454b638f90fd6
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2024-12-03 18:24:36 -0500

    oopsie

--- frontend/src/app/settings/SettingsPage.tsx
+++ frontend/src/app/settings/SettingsPage.tsx
@@ -408,6 +408,11 @@ export default function SettingsPage() {
               </AlertDialogContent>
             </AlertDialog>
           </div>
+          <Link href="/reports" passHref>
+            <Button variant="link" className="absolute top-2 left-0">
+              <ArrowLeft className="w-5 h-5 text-gray-400" />
+            </Button>
+          </Link>
         </div>
 
         {/* Account Deleted Dialog */}
