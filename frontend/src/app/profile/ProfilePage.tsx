69f8a8e5a14a82245ba8665f31495897409f1878
pkurto16 <ptkurto3@gmail.com>
2024-12-02 22:33:10 -0500

    Prittier

--- frontend/src/app/profile/ProfilePage.tsx
+++ frontend/src/app/profile/ProfilePage.tsx
@@ -409,11 +409,12 @@ export default function ProfilePage() {
           </div>
           <div>
             <div className="bg-slate-100 p-4 rounded-lg shadow-sm flex justify-between items-start drop-shadow-lg">
-
-            <ConnectComponentsProvider connectInstance={stripeConnectInstance}>
-              <ConnectAccountOnboarding onExit={() => {}} />
-            </ConnectComponentsProvider>
-          </div>
+              <ConnectComponentsProvider
+                connectInstance={stripeConnectInstance}
+              >
+                <ConnectAccountOnboarding onExit={() => {}} />
+              </ConnectComponentsProvider>
+            </div>
           </div>
         </div>
         <div className="flex h-16"></div>
