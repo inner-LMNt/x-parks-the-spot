f2232292a4e8e56824f29aae9cd637b871872329
pkurto16 <ptkurto3@gmail.com>
2024-12-03 22:34:05 -0500

    Color changes

--- frontend/src/app/search/components/driverArrive.tsx
+++ frontend/src/app/search/components/driverArrive.tsx
@@ -126,7 +126,7 @@ const DriverArrive = ({
         <DialogHeader>
           <DialogTitle>Spot Verification</DialogTitle>
         </DialogHeader>
-        <div className="text-center">
+        <div className="text-center text-slate-950">
           <p>Is this spot taken, or are you parked here?</p>
 
           {photoTimestamp && (
@@ -180,7 +180,7 @@ const DriverArrive = ({
                 onChange={() => setStatusSelection("taken")}
                 className="form-radio h-5 w-5 text-green-500"
               />
-              <span>This Spot was Already Taken</span>
+              <span className="text-slate-950">This Spot was Already Taken</span>
             </label>
             <label className="flex items-center space-x-2">
               <input
@@ -190,7 +190,7 @@ const DriverArrive = ({
                 onChange={() => setStatusSelection("parked")}
                 className="form-radio h-5 w-5 text-blue-500"
               />
-              <span>I Am Parked Here</span>
+              <span className="text-slate-950">I Am Parked Here</span>
             </label>
           </div>
 
@@ -203,7 +203,7 @@ const DriverArrive = ({
             >
               Submit
             </Button>
-            <Button onClick={closeDriverArriveDialog} variant="outline">
+            <Button onClick={closeDriverArriveDialog} variant="outline" className="text-slate-950">
               Close
             </Button>
           </div>
