662e095254c5486d3224dbd2a57fc849fb3845d3
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2024-12-03 22:06:19 -0500

    ui fixes

--- frontend/src/app/conflict/components/[user-id]/UserInfo.tsx
+++ frontend/src/app/conflict/components/[user-id]/UserInfo.tsx
@@ -80,26 +80,30 @@ const UserInfo: React.FC<{ userId: string; userDetails?: UserDetails }> = ({
         {/* Content Section */}
         <div className="p-6">
           <Tabs defaultValue="pastBookings">
-            <TabsList className="flex space-x-4 border-b border-gray-200 mb-4">
+            <TabsList
+                className="flex space-x-4 border-b border-gray-200 flex-1 mb-4 py-4 w-full bg-gray-100 rounded-lg"
+                style={{ minHeight: '3 rem' }}
+            >
               <TabsTrigger
-                value="pastBookings"
-                className="text-gray-700 hover:text-gray-900 focus:outline-none"
+                  value="pastBookings"
+                  className="w-32 text-gray-700 hover:text-gray-900 focus:outline-none text-center whitespace-normal py-2 px-3 leading-6"
               >
                 Past Bookings
               </TabsTrigger>
               <TabsTrigger
-                value="parkingSpaces"
-                className="text-gray-700 hover:text-gray-900 focus:outline-none"
+                  value="parkingSpaces"
+                  className="w-32 text-gray-700 hover:text-gray-900 focus:outline-none text-center whitespace-normal py-2 px-3 leading-6"
               >
                 Parking Spaces
               </TabsTrigger>
               <TabsTrigger
-                value="reports"
-                className="text-gray-700 hover:text-gray-900 focus:outline-none"
+                  value="reports"
+                  className="w-32 text-gray-700 hover:text-gray-900 focus:outline-none text-center whitespace-normal py-2 px-3 leading-6"
               >
                 Reports
               </TabsTrigger>
             </TabsList>
+
 
             {/* Past Bookings Tab */}
             <TabsContent value="pastBookings">
