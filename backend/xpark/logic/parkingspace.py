a46bbd03abe713040f1db1af6df8948621527660
Ersei Saggi <vcs@ersei.net>
2024-12-03 22:20:37 -0500

    Set timezone in auto spot log

--- backend/xpark/logic/parkingspace.py
+++ backend/xpark/logic/parkingspace.py
@@ -351,7 +351,7 @@ def create_free_parking_space(
         photos = []
 
     # Provide default values
-    default_name = f'Spot Logged at {datetime.now().strftime("%I:%M %p, %B %d %Y")}'
+    default_name = f'Spot Logged at {datetime.now().astimezone().strftime("%I:%M %p, %B %d %Y")}'
     default_verification_status = "unverified"
     default_availability_schedule = json.dumps([])  # or another appropriate default
     default_price = 0
